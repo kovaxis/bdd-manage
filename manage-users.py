ce65fe73d9e9ca0bdaa9fafd9a6deee4b792dfc9
kovaxis <martin.andrighetti@gmail.com>
2024-08-26 12:02:12 -0400

    user scanning

--- manage-users.py
+++ manage-users.py
@@ -7,12 +7,15 @@ import argparse
 import contextlib
 import csv
 from dataclasses import dataclass
+import dataclasses
+from datetime import datetime
 from pathlib import Path
 import re
 import sys
 import subprocess
 import os
 import traceback
+from hashlib import blake2b as good_hash
 
 
 def parse_args() -> argparse.Namespace:
@@ -30,13 +33,14 @@ def parse_args() -> argparse.Namespace:
     - lock: Bloquea la cuenta de todos los usuarios de la lista.
     - unlock: Desbloquea las cuentas de todos los usuarios de la lista.
     - kick: Detiene las conexiones actuales de todos los usuarios de la lista.
+    - scan: Escanear las carpetas HOME y recordar información sobre la fecha de entrega.
     """,
         formatter_class=argparse.RawTextHelpFormatter,
     )
     p.add_argument("lista_alumnos", help="Archivo .csv con la lista de alumnos")
     p.add_argument(
         "accion",
-        choices=["create", "destroy", "lock", "unlock", "kick"],
+        choices=["create", "destroy", "lock", "unlock", "kick", "scan"],
         help="Qué acción realizar",
     )
     p.add_argument(
@@ -56,6 +60,18 @@ class User:
     n_alumno: str
     run: str = ""
     section: str = ""
+
+
+@dataclass
+class UserScan:
+    """
+    Datos de entrega de un usuario.
+    """
+
+    id: str
+    ultimo_cambio_detectado: datetime
+    hash: str
+    mtime: datetime
 
 
 @contextlib.contextmanager
@@ -148,6 +164,69 @@ def read_users(list_path: Path) -> dict[str, User]:
         sys.exit(1)
 
 
+def read_userscan(path: Path) -> dict[str, UserScan]:
+    """
+    Leer un CSV de scans de usuarios.
+    """
+    with exception_context(f"leyendo csv de userscan en {path}"):
+        userscan = {}
+        fields = dataclasses.fields(UserScan)
+        with open(path, encoding="utf-8", newline="") as file:
+            for row in csv.DictReader(file):
+                args = {}
+                for field in fields:
+                    if field.name in row:
+                        x = row[field.name]
+                        if field.type is datetime:
+                            x = datetime.fromisoformat(x)
+                        args[field.name] = x
+                user = UserScan(**args)
+                userscan[user.id] = user
+        return userscan
+
+
+def write_userscan(path: Path, userscan: dict[str, UserScan]):
+    with exception_context(f"escribiendo csv de usercan en {path}"):
+        with open(path, encoding="utf-8", newline="") as file:
+            fields = dataclasses.fields(UserScan)
+            writer = csv.DictWriter(file, fieldnames=[field.name for field in fields])
+            writer.writeheader()
+            for user in userscan.values():
+                row = {}
+                for field in fields:
+                    x = getattr(user, field.name)
+                    if field.type is datetime:
+                        x = datetime.isoformat(x)
+                    row[field.name] = x
+                writer.writerow(row)
+
+
+def hash_filesystem(path: Path, root: Path | None = None) -> bytes:
+    if root is None:
+        root = path
+    hx = good_hash()
+    hx.update(b"d" if path.is_dir() else b"f")
+    hx.update(path.relative_to(root).as_posix().encode())
+    if path.is_dir():
+        hashes: list[str] = []
+        for subfile in path.iterdir():
+            hashes.append(hash_filesystem(subfile))
+        hashes.sort()
+        for h in hashes:
+            hx.update(h)
+    else:
+        hx.update(path.read_bytes())
+    return hx.digest()
+
+
+def get_mtime(path: Path) -> datetime:
+    t = datetime.fromtimestamp(path.lstat().st_mtime)
+    if path.is_dir():
+        for subpath in path.iterdir():
+            t = max(t, get_mtime(subpath))
+    return t
+
+
 if __name__ == "__main__":
     conf = parse_args()
     users = read_users(Path(__file__).with_name(conf.lista_alumnos))
@@ -193,8 +272,12 @@ if __name__ == "__main__":
         case "destroy":
             # Codigo para destruir los usuarios
             for user in users.values():
-                subprocess.run(["sudo", "deluser", user.id, "--remove-home"])
-                print(f"deleted user {user.id}")
+                try:
+                    subprocess.run(["sudo", "deluser", user.id, "--remove-home"])
+                    print(f"deleted user {user.id}")
+                except subprocess.CalledProcessError:
+                    traceback.print_exc()
+                    print(f"failed to destroy user {user.id}")
         case "lock":
             # Codigo para bloquear los usuarios
             for user in users.values():
@@ -210,6 +293,40 @@ if __name__ == "__main__":
             for user in users.values():
                 subprocess.run(["killall", "-9", "--user", user.id])
                 print(f"killed processes of user {user.id}")
+        case "scan":
+            # Código para escanear las carpetas HOME y determinar horas de entrega
+            scantime = datetime.now()
+            path = Path(conf.lista_alumnos)
+            path = path.with_stem(path.stem() + "_scan")
+            userscan = read_userscan(path)
+            try:
+                for user in users.values():
+                    subprocess.run(["sudo", "passwd", "-l", user.id])
+                print("locked all users")
+                for user in users.values():
+                    home = Path(f"/home/{user.id}")
+                    h = hash_filesystem(home).hex()
+                    mtime = get_mtime(home)
+                    old = userscan.get(user.id)
+                    oldh = None if old is None else old.hash
+                    change = scantime if h != oldh else old.ultimo_cambio_detectado
+                    userscan[user.id] = UserScan(
+                        id=user.id,
+                        ultimo_cambio_detectado=change,
+                        hash=h,
+                        mtime=mtime,
+                    )
+                    print(f"scanned user {user.id}")
+                write_userscan(path, userscan)
+            finally:
+                for user in users.values():
+                    try:
+                        for user in users.values():
+                            subprocess.run(["sudo", "passwd", "-u", user.id])
+                    except subprocess.CalledProcessError:
+                        traceback.print_exc()
+                print("unlocked users")
+
         case _:
             raise RuntimeError(f"unknown action {conf.accion}")
 
